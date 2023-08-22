c9219bbba4de3d7ee99ceb8158162631eda01df7
Joseph Silva <119358195+JosephSC0121@users.noreply.github.com>
2023-08-22 18:59:11 -0500

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,47 @@
+import io from 'socket.io-client'
+import {useState, useEffect} from 'react'
+
+const socket = io("/")
+
+function App() {
+const [message, setMessage] = useState('')
+const [messages, setMessages] = useState([])
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const newMessage ={
+      body: message,
+      from: 'Me'
+    }
+    setMessages([...messages, newMessage])
+    socket.emit('message', message)
+  }
+  useEffect(() => {
+    socket.on('message', reciveMessage)    
+    return()=> {
+      socket.off('message', reciveMessage)
+    }
+  }, [])
+
+  const reciveMessage = (message) => setMessages(state => [...state, message])
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder='mesaje...' 
+        onChange={(e) => setMessage(e.target.value)}/>
+        <button>
+          Send
+        </button>
+      </form>
+      <ul>
+        {
+          messages.map((message, i) =>(
+            <li key={i}>{message.from}:{message.body}</li>
+          ))
+        }
+      </ul>
+    </div>
+  )
+}
+
+export default App
